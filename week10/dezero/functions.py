60823410bab1092f7d0e3d26b8e7e385fdca8a67
EunNamCho <jen9581@gmail.com>
2024-06-03 17:46:59 +0900

    [Study] week10: Recap

---
+++ week10/dezero/functions.py
@@ -0,0 +1,52 @@
+import numpy as np
+from dezero.core import Function
+
+class Sin(Function):
+    def forward(self, x):
+        return np.sin(x)
+    
+    def backward(self, gy):
+        return gy * cos(self.inputs[0])
+    
+class Cos(Function):
+    def forward(self, x):
+        return np.cos(x)
+    
+    def backward(self, gy):
+        return gy * -sin(self.inputs[0])
+    
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        return gy * np.exp(self.inputs[0])
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        return gy * 2 * self.inputs[0]
+    
+class Tanh(Function):
+    def forward(self, x):
+        return np.tanh(x)
+    
+    def backward(self, gy):
+        return gy * (1 - self.outputs[0]() * self.outputs[0]())
+    
+def exp(x):
+    return Exp()(x)
+
+def square(x):
+    return Square()(x)
+
+def sin(x):
+    return Sin()(x)
+
+def cos(x):
+    return Cos()(x)
+
+def tanh(x):
+    return Tanh()(x)
